702ccc84fffafd8e317d9d704bcc164ed5002d62
lee <lee@leedeMacBook-Pro.local>
2018-09-29 20:03:34 +0800

    新浪登录demo(未完成)

---
+++ Sina/Login/SDK_Sina_Login/SDK_Sina_Login/AppDelegate.m
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.m
+//  SDK_Sina_Login
+//
+//  Created by lee on 2018/9/27.
+//  Copyright © 2018年 lee. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+/**
+ 
+ http://open.weibo.com/apps/2510629175/info/basic
+ 新浪开放平台信息
+ App Key：2510629175
+ App Secret：a4166c5f12f175f2616cd6d7e5d2bba0
+ */
+
+@end
---
+++ Sina/Login/SDK_Sina_Login/SDK_Sina_Login/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SDK_Sina_Login
+//
+//  Created by lee on 2018/9/27.
+//  Copyright © 2018年 lee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+// 新浪登录
+- (IBAction)sina_login:(UIButton *)sender {
+    NSLog(@"新浪登录");
+}
+
+
+@end
