01b97bd48c62248672d63df454f42dbc897a74f6
lee <lee@leedeMacBook-Pro.local>
2018-11-24 16:02:55 +0800

    提交

---
+++ 网易_NTES_网易云信/直播/README.md
@@ -0,0 +1,20 @@
+# 直播
+一、词语说明
+    
+    推流 (主播)
+    拉流 (用户)
+   
+二、文档链接
+    
+    Demo 下载链接
+    http://vcloud.163.com/download
+    直播文档
+    https://dev.yunxin.163.com/docs/product/直播/播放器SDK/iOS播放器SDK/开发指南
+    直播开发手册
+    https://dev.yunxin.163.com/docs/product/直播/产品介绍/简介    
+    服务端api
+    https://dev.yunxin.163.com/docs/product/直播/服务端API文档
+    
+    
+    
+
---
+++ 网易_NTES_网易云信/直播/播放端/Project/Cocoapods集成/NTES_Player_CocoaPods/NTES_Player_CocoaPods/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NTES_Player_CocoaPods
+//
+//  Created by lee on 2018/11/17.
+//  Copyright © 2018年 lee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
